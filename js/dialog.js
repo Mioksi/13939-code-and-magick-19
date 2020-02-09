84733aa143b2c33340d3c8d8146f85b4adeab1db
Mioksi <mio.natali@hotmail.com>
2020-02-09 18:39:27 +0300

    Добавляет возможность перемещения диалога редактирования персонажа

--- js/dialog.js
+++ js/dialog.js
@@ -8,6 +8,7 @@
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
+  var dialogHandler = setup.querySelector('.upload');
   var userNameInput = setup.querySelector('.setup-user-name');
   var wizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
   var wizardEyes = setup.querySelector('.setup-wizard .wizard-eyes');
@@ -40,6 +41,11 @@
     changeColorElement(FIREBALL_COLORS, wizardFireball, wizardFireballInput);
   };
 
+  var resetSetupDialog = function () {
+    setup.style.left = '';
+    setup.style.top = '';
+  };
+
   var onPopupEscPress = function (evt) {
     if (evt.target !== userNameInput) {
       window.utils.isEscEvent(evt, closePopup);
@@ -59,6 +65,8 @@
   var closePopup = function () {
     setup.classList.add('hidden');
     setup.querySelector('.setup-similar').classList.add('hidden');
+
+    resetSetupDialog();
 
     document.removeEventListener('keydown', onPopupEscPress);
     wizardCoat.removeEventListener('click', onCoatChangeColor);
@@ -80,5 +88,52 @@
 
   setupClose.addEventListener('keydown', function (evt) {
     window.utils.isEnterEvent(evt, closePopup);
+  });
+
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var isDragged = false;
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      isDragged = true;
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (isDragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
   });
 })();
